de0a3200dfa174e93ec4dc832b2888c1ad8ca1b8
= <28835813+ploop3@users.noreply.github.com>
2023-09-20 16:02:30 +0200

    Import dev data from local json to database

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('../../models/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.PASSWORD_DATABASE,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log('DB connection successful'));
+
+//Read JSON file
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'),
+);
+
+//Import data into DB
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully loaded!');
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
+};
+
+//Delete all tours
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany({});
+    console.log('Data successfully removed!');
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
