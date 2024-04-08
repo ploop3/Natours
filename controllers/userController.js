cd7bb014551766f507c2f3afb43e4c828884c671
pedro <28835813+ploop3@users.noreply.github.com>
2024-04-08 13:41:14 +0200

    Allow users to update their own user name and email address

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,23 @@
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+/**
+ *
+ * @param {*} obj = the req.body with all properties that come from the end user
+ * @param  {...any} allowedFields are the only ones that the user can modify from the DB
+ * Otherwise, without filtering, they could assign themselves a new role
+ * @returns The filtered object with the properties to update the DB
+ */
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((element) => {
+    if (allowedFields.includes(element)) {
+      newObj[element] = obj[element];
+    }
+  });
+  return newObj;
+};
 
 /**
  * Route andlers
@@ -14,6 +32,40 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
     results: users.length,
     data: {
       users,
+    },
+  });
+});
+
+/**
+ * Update only your own name and email address
+ */
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1) Create error is user POSTs password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password updates. Please use /updateMyPassword',
+        400,
+      ),
+    );
+  }
+
+  // 2) Filtered out unwanted field names that are not allowed to be updated
+  const filteredBody = filterObj(req.body, 'name', 'email');
+
+  // 3) Update user document
+  //This time we can't use findByIdAndUpdate because we don't need to compare password and
+  // passwordConfirm, since we are dealing with not sensitive data
+
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser,
     },
   });
 });
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -18,6 +18,7 @@ router.patch(
   authController.protect,
   authController.updatePassword,
 );
+router.patch('/updateMe', authController.protect, userController.updateMe);
 
 router
   .route('/')
