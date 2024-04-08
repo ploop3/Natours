7b279bdbf0822eb1755c8037fc42fa9db49f3645
pedro <28835813+ploop3@users.noreply.github.com>
2024-04-08 19:00:47 +0200

    Send JsonWebToken via cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,6 +16,22 @@ const signToken = (id) => {
 const createAndSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
 
+  //Expires in X Days converted to milliseconds
+  //httpOnly = the cookie cannot be accessed/modified by the broser (only server) -cross XSS
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
+    ),
+    secure: false,
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+  res.cookie('jwt', token, cookieOptions);
+
+  //Remove password from the output
+  user.password = undefined;
+
   res.status(statusCode).json({
     status: 'success',
     token,
