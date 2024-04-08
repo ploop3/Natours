641a46590a907a7553be9b4b3ae6396b78620b13
pedro <28835813+ploop3@users.noreply.github.com>
2024-04-08 21:51:59 +0200

    Implement Security - Rate limiting Limits each IP to 100 requests per window (15 minutes in my code)
    - Setting Security HTTP Headers with helmet
    - Data Sanitization against noSQL query injection with express-mongo-sanitize
    - Data Sanitization against XSS with xss-clean
    - Prevent parameter pollution with hpp

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const globalErrorHandler = require('./controllers/errorController');
 const AppError = require('./utils/appError');
@@ -9,17 +14,69 @@ const userRouter = require('./routes/userRoutes');
 //Will add a bunch of methods to our app variable
 const app = express();
 
-//Middlewares
+// 1) GLOBAL Middlewares
+
+//Set security HTTP headers
+app.use(helmet());
+
+//Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json());
+//Limits each IP to 100 requests per window (15 minutes)
+const limiter = rateLimit({
+  limit: 100,
+  windowMs: 15 * 60 * 1000,
+  message: {
+    status: 'fail',
+    data: {
+      message: 'Too many requests from this IP, please try again in an hour!',
+    },
+  },
+});
+
+//Will apply only to the routes that start with /api
+app.use('/api', limiter);
+
+//Body parser, reading data from body into req.body
+//Limit the size of the body, if it's larger it won't be accepted
+app.use(
+  express.json({
+    limit: '10kb',
+  }),
+);
+
+//Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+//Data sanitization against XSS
+app.use(xss());
+
+//Prevent parameter pollution
+//Whitelist parameters to be allowed
+//localhost:3000/api/v1/tours/?duration=5&duration=9
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsAverage',
+      'ratingsQuantity',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  }),
+);
+
+//Serving static files
 app.use(express.static(`${__dirname}/public/`));
 
+// 2) ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+// 3) Capture errors
 //Middleware to caputure unhandled routes.
 //If it reached this point it means it was not for tours nor users
 app.all('*', (req, res, next) => {
