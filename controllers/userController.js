aef78b85b6eac0590189b091d71dd8b3ca6c860f
pedro <28835813+ploop3@users.noreply.github.com>
2024-04-08 18:35:37 +0200

    Delete current user

--- controllers/userController.js
+++ controllers/userController.js
@@ -70,6 +70,19 @@ exports.updateMe = catchAsync(async (req, res, next) => {
   });
 });
 
+/**
+ * Delete your own account.
+ * We don't really delete it from DB, oly change the property active = false
+ */
+exports.deleteMe = catchAsync(async (req, res, next) => {
+  await User.findByIdAndUpdate(req.user.id, { active: false });
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
+
 exports.getUser = (req, res) => {
   res.status(500).json({
     status: 'error',
--- models/userModel.js
+++ models/userModel.js
@@ -41,6 +41,11 @@ const userSchema = new mongoose.Schema({
   passwordChangedAt: Date,
   passwordResetToken: String,
   passwordResetExpires: Date,
+  active: {
+    type: Boolean,
+    default: true,
+    select: false, //to show it in the user properties only if explicitly requested +active
+  },
 });
 
 /**
@@ -70,6 +75,19 @@ userSchema.pre('save', function (next) {
   //because of our checking code.
   //One solution is to simply rest one second
   this.passwordChangedAt = Date.now() - 1000;
+  next();
+});
+
+/**
+ * Query middleware.
+ * Run whenever we query an user using any method that starts with '/^find/'
+ * The goal is to hide the users where "active = false"
+ */
+
+userSchema.pre(/^find/, function (next) {
+  //this points to the current query
+  //Will return all documents where active is '$not equal' to false
+  this.find({ active: { $ne: false } });
   next();
 });
 
