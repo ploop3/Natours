247314b65a4e02779e8b551a5792464d073d1dd2
pedro <28835813+ploop3@users.noreply.github.com>
2024-05-22 18:38:26 +0200

    Creating a MongoDB Booking entry at stripe checkout success

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,5 +1,6 @@
 const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY);
 const Tour = require('../models/tourModel');
+const Booking = require('../models/bookingModel');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handleFactory');
 const AppError = require('../utils/appError');
@@ -10,11 +11,12 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 
   //2. Create Stripe product and Stripe Payment objects
 
-  //3. create checkout sessoin
+  //3. create checkout session
+  //Insecure way to confirm a booking using query params
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
     mode: 'payment',
-    success_url: `${req.protocol}://${req.get('host')}/`,
+    success_url: `${req.protocol}://${req.get('host')}/?tour=${req.params.tourId}&user=${req.user.id}&price=${tour.price}`,
     cancel_url: `${req.protocol}://${req.get('host')}/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
@@ -32,4 +34,19 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     session,
   });
   // res.redirect(303, 'http://localhost:3000')
+});
+
+/**
+ * TEMP solution
+ * We will hit this endpoint twice, one with all three query params
+ * to create the booking in the DB, and the second without params
+ * to show the overview page(next middleware)
+ */
+exports.createBookingCheckout = catchAsync(async (req, res, next) => {
+  const { tour, user, price } = req.query;
+  if (!tour && !user && !price) return next();
+  await Booking.create({ tour, user, price });
+
+  //redirect to stripe success_url
+  res.redirect(req.originalUrl.split('?')[0]);
 });
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const viewController = require('../controllers/viewsController');
 const authController = require('../controllers/authController');
+const bookingController = require('../controllers/bookingController');
 
 const router = express.Router();
 
 // router.use(authController.isLoggedIn);
 
-router.get('/', authController.isLoggedIn, viewController.getOverview);
+router.get(
+  '/',
+  bookingController.createBookingCheckout,
+  authController.isLoggedIn,
+  viewController.getOverview,
+);
 router.get('/tour/:slug', authController.isLoggedIn, viewController.getTour);
 router.get('/login', authController.isLoggedIn, viewController.getLoginForm);
 
