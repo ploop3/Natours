8883eaef5528f91f4a7874ceecc07698c59be9b5
pedro <28835813+ploop3@users.noreply.github.com>
2024-05-24 08:35:32 +0200

    Add CRUD methods for bookings API

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -4,11 +4,22 @@ const authController = require('../controllers/authController');
 
 const router = express.Router();
 
+router.use(authController.protect);
+
 //So the frontend gets a stripe checkout session
-router.get(
-  '/checkout-session/:tourId',
-  authController.protect,
-  bookingController.getCheckoutSession,
-);
+router.get('/checkout-session/:tourId', bookingController.getCheckoutSession);
+
+router.use(authController.restrictTo('admin', 'lead-guide'));
+
+router
+  .route('/')
+  .get(bookingController.getAllBookings)
+  .post(bookingController.createBooking);
+
+router
+  .route('/:id')
+  .get(bookingController.getBooking)
+  .patch(bookingController.updateBooking)
+  .delete(bookingController.deleteBooking);
 
 module.exports = router;
